97c65e70277b9e14ad0483d076be13c23235d6ba
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2019-09-14 00:30:47 -0400

    Minor updates and to-do list in hitting times funciton.

--- hitting_time_functions.R
+++ hitting_time_functions.R
@@ -14,19 +14,31 @@ if (require('dplyr')==FALSE) {
   require(dplyr)
 }
 
+# To-do:
+#  Add ROxygen documentation?
+#  Make pop. simulation more general.
+#  Update censor function to bin with hist()?
+#  Set up log_log_lm to accept a data frame OR two vectors.
+#  Write a wrapper function that takes as input a simulation function and 
+#   its parameters, and n, replicates it n times, does the log-log model and
+#   returns the coefficients.
+
 simulate_individual_hitting_time <- function(n, lambda) {
-  # Inputs: n is an integer, lambda is numeric.
+  # Inputs: n is an integer, lambda is numeric scalar.
   simulated_times <- rexp(n = n, rate = lambda)
   maximum_simulated_time <- max(simulated_times)
   return(maximum_simulated_time)
-  # Output is numeric
+  # Output is numeric scalar.
 }
 
 simulate_population_hitting_time <- function(n, lambda, trials) {
   # Inputs: n is an integer, lambda is numeric, trials is an integer.
+  # Need to update this so it more generally takes in an individual simulation
+  #  function and its parameters, instead of only this specific Armitage-Doll
+  #  simualtion function.
   res <- replicate(trials, hitting_time(n, lambda))
   return(res)
-  # Outputs a numeric vector
+  # Outputs a numeric vector.
 }
 
 censor <- function(data, bins) {
@@ -56,12 +68,26 @@ censor <- function(data, bins) {
   return(censored_data)
 }
 
-# lm(log(count)~log(midpoints)) from censor output
+# Take in two numeric vectors IN ORDER and compute the log of both vectors. 
+#  Then, fit a linear model to the logs. This essentially fits a power law
+#  model to the data.
 log_log_lm <- function(x,y) {
   logx <- log(x)
   logy <- log(y)
   model <- lm(logy~logx)
   return(model)
+}
+
+# Take in a log-log linear model object, extract coefficients and return
+#  "un-transformed coefficients".
+
+untransform_power_law_coefs <- function(model) {
+  coefs <- model$coefficients #entry one is the intercept, two is the slope
+  # Recall a power law is of the form y = ax^b
+  scale_parameter <- e^coefs[[1]] # This "un-transforms" the a parameter.
+  exponent <- coefs[[2]] # Doesn't need to be "un-transformed".
+  output_vector <- list(scale_parameter,exponent)
+  return(output_vector)
 }
 
 # End of file.
